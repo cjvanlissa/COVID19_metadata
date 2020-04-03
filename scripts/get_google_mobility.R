d6de407c8c302609b239d6026a1398155c06d626
cjvanlissa <c.j.vanlissa@uu.nl>
2020-04-03 14:28:07 +0200

    Update google scraping

--- scripts/get_google_mobility.R
+++ scripts/get_google_mobility.R
@@ -6,13 +6,29 @@ get_file_mobility <- function(){
   
   files <- lapply(file_list, textreadr::read_pdf)
   
-  for(lines in files[1:3]){
+  tmp <- sapply(files, function(lines){
     #lines <- files[[2]]
     lines <- lines$text
     headers <- lines[grep("\\+80%", lines)-1]
-    values <- lines[grep("^[\\+-]{0,1}\\d+%$", lines)]
-    values <- values[!grepl("(80|40)", values)]
-    if(!length(headers) == length(values)) stop("Couldn't extract headers/values")
-  }
+    values <- lines[grep("(^[\\+-]{0,1}\\d+%|ot enough data)", lines)]
+    values1 <- values[!grepl("(80|40)", values)]
+    values2 <- values[grep("\\+80%", values)+1]
+    if(length(values1) >= length(values2)){
+      values <- values1
+    } else {
+      values <- values2
+    }
+    if(length(values) > 6){
+      if(grepl("ot enough data", values[7])){
+        values <- values[1:6]
+      } else {
+        # Fix a problem
+        browser()
+      }
+    }
+    names(values) <- headers
+    values
+  })
   
+  tmp2 <- sapply(tmp, function(x){x[1:18]})
 }
