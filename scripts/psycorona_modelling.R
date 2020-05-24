132973c15d94ebc2a2e3353b5da31f0c5650df82
AndriiG13 <andriigrygoryshyn4@gmail.com>
2020-05-24 10:38:23 +0200

    vv

--- scripts/psycorona_modelling.R
+++ scripts/psycorona_modelling.R
@@ -1,12 +1,7 @@
 ########## TO DO's ##########
 
-# TODO: PRIORITY: implement missRanger to impute NAs (only ~10 rows without NAs, need complete rows for models)
-
 # TODO: list all outcome variables that we may want to use; organise script (modelling, evaluation) accordingly
 # TODO: clean datafile for multiple entries for a single participant (see exploration script TK SHOULD BE CLEANED)
-# TODO: clean data according to parameters above; only exclude potential predictors and outcome variables
-# TODO: make sure that all predictors are in the right format: factors for categorical variables, otherwise doubles
-# TODO: make test / train / validation sets using random set.seed(953007)
 # TODO: make different versions of those sets to fit the various packages: data frames, matrices...
 # TODO: add models: Lasso, Elastic Net, RandomForest, KNN?
 # TODO: add different evaluation metrics, evaluate each model on each metric
@@ -14,124 +9,168 @@
 
 ########## PREPARATION ##########
 
-library(tidyverse)
-
-source("scripts/psyCorona_exploration.R") # get (subsets of) raw data from exploration script
-
-# Parameters
-partic_NA_tres_perc     <- 10 # remove participants that have more than X% NAs
-partic_NA_tres_var      <- 15 # remove features/variables that have more than X% NAs
-country_remove_treshold <- .5 # remove countries that account for less than X% of all rows
-p_train                 <- .7 # percentage of data in training set
-p_test                  <- .2 # percentage of data in test set
-p_validation            <- .1 # percentage of data in validation set
-
-########## ALLOCATING VARIABLES ##########
-
-# Outcome variables (self-containment, social distancing, and attitudes toward government policies)
-handwashing  <- "c19perBeh01" # wash hands more often to lower chances of getting corona [7-point Likert; -3:3]
-avoid_crowds <- "c19perBeh02" # avoid crowds to lower chances of getting corona [7-point Likert; -3:3]
-self_quarant <- "c19perBeh03" # self-quarantine to lower chances of getting corona [7-point Likert; -3:3]
-houseleave   <- "houseleave"  # how often participant left house in the past week
-# tk make this nicer, add coronaclose etc.
+# library(tidyverse)
+library(Matrix)
+  
+#source("scripts/psyCorona_exploration.R") # get (subsets of) raw data from exploration script
+df_analyse <- read.csv("training.csv", stringsAsFactors = FALSE)
 
 ########## PREPARE DATA ##########
 
-# change the type to integer where appropriate
-# first get the list of columns that already contain integer values 
-int_columns_list = c()
-for (col_n in 1:dim(df_analyse)[2]) {
-  
-  current_column_values <-  df_analyse[col_n]
-  current_column_name <- colnames(df_analyse[col_n])
-  
-  if (is.numeric(current_column_values[1, ])) {
-    if (all((current_column_values%%1 == 0) == TRUE, na.rm=T)) {
-      int_columns_list = c(int_columns_list, current_column_name)}
-  }
-}
-# change the type to integer for the appropriate columns 
-df_analyse <- df_analyse %>% mutate_at(int_columns_list, as.integer)
+# Removing NA participants & vars
+partic_NA_tres_perc     <- .2 # remove participants that have more than X% NAs
+partic_NA_tres_var      <- .2 # remove features/variables that have more than X% NAs
 
-# Impute NAs with missranger
+miss       <- is.na(df_analyse)
+df_analyse <- df_analyse[!(rowSums(miss)/ncol(df_analyse)) > partic_NA_tres_perc,
+                         -which((colSums(miss)/nrow(df_analyse)) > partic_NA_tres_var)]
+
+desc <- descriptives(df_analyse)
+
+# Median/mode substitution until we implement a better single imputation method
+df_analyse[, desc$type == "integer"] <- lapply(desc$name[desc$type == "integer"], function(varname){
+  out <- df_analyse[[varname]]
+  out[is.na(out)] <- desc$mode[desc$name == varname]
+  out
+})
+
+df_analyse[, desc$type == "numeric"] <- lapply(desc$name[desc$type == "numeric"], function(varname){
+  out <- df_analyse[[varname]]
+  out[is.na(out)] <- desc$median[desc$name == varname]
+  out
+})
+
+
+# Impute NAs with missranger - this doesn't work now
 # get number of non NAs per row to use as case.weights
-non_miss <- rowSums(!is.na(df_analyse))
+#non_miss <- rowSums(!is.na(df_analyse))
 # fitting missRanger
-df_analyse_NA_imputed <- missRanger(df_analyse %>% dplyr::select(-x1), num.trees = 100, pmm.k = 3,
-                                    case.weights=non_miss)
+# set.seed(3609)
+# df_analyse_NA_imputed <- missRanger(df_analyse)
 # quick check to see if any NAs are left over
-any(rowSums(is.na(df_analyse_NA_imputed)) > 0)
+if(anyNA(df_analyse)) stop("Still NAs in df_analyse.")
 
 
-########## ADD OUTCOME VARIABLES ##########
+# Clearing environment
+env_obj <- ls()
+env_obj <- env_obj[!env_obj == "df_analyse"]
+do.call(rm, as.list(env_obj))
+gc()
 
-# Adding outcome variables and getting data ready for modelling
-# OUTCOME 1: Predicting whether people know anyone who has COVID (including themselves)
+########## ALLOCATING VARIABLES ##########
 
-# I chose to set a "coronaKnow" variable which is 1 if the participants knows anyone with corona (including themselves)
-# and is 0 if they do not. It's arguable if this is best, but gives us the most positive cases to work with.
-df_mod <- df_analyse_NA_imputed %>% mutate(knowcorona = as.factor(ifelse(coronaclose == 6, 0, 1)))
+# Remove variables that we don't use as predictors or outcomes
+df_analyse <- df_analyse[,-which(names(df_analyse) %in% c("responseid", "recordeddate", "train", "coronaclose"))]
 
-# create final data set for modelling and assign train, test, or validation set
-set.seed(953007)
-df_mod_final <- df_mod %>% 
-  # dplyr::select(-coronaclose_1,-coronaclose_2,  -coronaclose_3,  -coronaclose_4,  -coronaclose_5, -coronaclose_6) %>% 
-  mutate_at(vars_to_encode[12:17], funs(factor(.))) %>% 
-  mutate(set = sample.int(3, nrow(df_mod), replace = TRUE, prob = c(p_train, p_test, p_validation)))
+# Let's predict self-serving behavior as an example / template ("knowcorona" code is down below)
+outcome_vars <- c("c19perbeh") # hand washing, avoiding crowds, self-quarantining
+preds        <- names(df_analyse)[-(which(names(df_analyse) %in% outcome_vars))]
 
-df_mod_final$coronaclose_1 # check
-
-# put startDate into bins. The range is 5 weeks, so I will use 5 bins
-df_mod_final$startdate <- cut(df_mod_final$startdate, breaks = 5, 
-                               labels = c("week_1", "week_2", "week_3", "week_4", "week_5"))
-
-# Splitting the data into train, test, and validation set
-df_train <- df_mod_final %>% filter(set == 1) %>% select(-set)
-df_test  <- df_mod_final %>% filter(set == 2) %>% select(-set)
-df_val   <- df_mod_final %>% filter(set == 3) %>% select(-set)
-
-# Making model matrices of training set for each outcome variable (needed for e.g. Lasso)
-m_train <- model.matrix(knowcorona ~ .-startdate, data = select(df_train, -x1))
-
-# Extract outcome variables of
-y_train = as.factor(df_train$knowcorona)
+# Explore outcome variable
+hist(df_analyse$c19perbeh)  # people tend to self-serve
 
 ########## MODELLING ##########
 
-# Outcome 1: knowcorona
-knowcorona_mod_lasso <- cv.glmnet(x = m_train, y = y_train, family = "binomial", type.measure = 'auc', alpha = 1, 
-                                  nfolds = 5)
+### Outcome 1: c19perbeh
+x_train_c19perbeh <- sparse.model.matrix(c19perbeh ~ ., data = df_analyse) # making model matrix
+y_train_c19perbeh <- df_analyse$c19perbeh                                  # extract outcome variable
+
+# Lasso model
+m_lasso_c19perbeh <- cv.glmnet(x = x_train_c19perbeh, 
+                               y = y_train_c19perbeh, 
+                               family = "gaussian", 
+                               type.measure = 'mse', 
+                               alpha = 1, 
+                               nfolds = 10)
 
 ########## EVALUATION ##########
 
-### Outcome 1: knowcorona
+### Outcome 1: c19perbeh
 
-# Plots
-plot(knowcorona_mod_lasso)
+# Exploring Lasso model
+plot(m_lasso_c19perbeh)      # choosing the optimal Lambda
+m_lasso_c19perbeh$lambda.min # chosen lambda
 
-# ...
-mean(predict(knowcorona_mod_lasso, newx = train_matrix_x, type="class", 
-             s = knowcorona_mod_lasso$lambda.1se) == y_train) # train set
+# Evaluating model on training set (cross-validation)
+m_lasso_c19perbeh$cvm[which(m_lasso_c19perbeh$lambda == m_lasso_c19perbeh$lambda.min)] # mean squared error on train set
 
-lasso_preds <- predict(knowcorona_mod_lasso, newx=train_matrix_x, type="class",
-                       s=knowcorona_mod_lasso$lambda.1se)
+# Get the coefficients and make them into a data frame
+lasso_coefs        <- coef.glmnet(m_lasso_c19perbeh, s = m_lasso_c19perbeh$lambda.min)
+lasso_coefs_excl   <- lasso_coefs[which(lasso_coefs[,1] == 0), 1]    # these features have been excluded
+lasso_coefs_select <- lasso_coefs[-(which(lasso_coefs[,1] == 0)), 1] # these features have been selected (= meaningful)
+sort(abs(lasso_coefs_select), decreasing = TRUE) # coefs sorted according to importance
 
-lasso_preds_processed <- as.factor(unname(lasso_preds[,1]))
+# Coef plot
+coefs <- lasso_coefs_select[!names(lasso_coefs_select) == "(Intercept)"]
+plotdat <- data.frame(var = ordered(names(coefs), levels = names(coefs)[order(abs(coefs))]),
+           val = abs(coefs),
+           rank = seq_along(coefs))
 
-# Accuracy, precision, recall, F1...
-precision <- posPredValue(lasso_preds_processed, y_train, positive="1")
-precision
-recall <- sensitivity(lasso_preds_processed, y_train, positive="1")
-recall
-F1 <- (2 * precision * recall) / (precision + recall)
-F1
+ggplot(plotdat,
+       aes(x = 0, xend = val, y = var, yend = var)) + geom_segment() + geom_point(aes(x = val, y = var))  +theme_bw()
 
-# get the coefficients and make them into a tibble
-lasso_coefs <- coef.glmnet(knowcorona_mod_lasso, s=knowcorona_mod_lasso$lambda.min)
-coefs_df <- tibble(var_name = names(lasso_coefs[,1]), coef = unname(lasso_coefs[,1]))
+source("scripts/model_accuracy.R")
+model_accuracy(m_lasso_c19perbeh, 
+               observed = y_train_c19perbeh,
+               olddata = x_train_c19perbeh)
 
-# plot coefficients above .3
-coefs_df %>% filter(abs(coef) > 0.3) %>% 
-  ggplot(aes(x=var_name, y=coef)) +
-  geom_bar(stat="identity") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+library(ranger)
+library(metaforest)
+
+res <- ranger(y = y_train_c19perbeh, x = x_train_c19perbeh, importance = "permutation")
+VarImpPlot(res)
+
+
+
+
+# CJ: Don't run this code yet, BUT it fails because different variables are selected in train and test. We should make ALL variable selection decisions in the exploration script, not in the modeling script.
+# df_testing <- read.csv("testing.csv", stringsAsFactors = FALSE)
+# ndat <- sparse.model.matrix(c19perbeh ~ ., data = df_testing)
+# all(colnames(ndat) %in% colnames(x_train_c19perbeh))
+# colnames(ndat)[!colnames(ndat) %in% colnames(x_train_c19perbeh)]
+# ndat <- ndat[, match(colnames(x_train_c19perbeh), colnames(ndat))]
+
+model_accuracy(m_lasso_c19perbeh, 
+               newdata = ndat,
+               observed = y_train_c19perbeh,
+               olddata = x_train_c19perbeh)
+# coefs_df <- data.frame(var_name = names(lasso_coefs[,1]), coef = unname(lasso_coefs[,1]))
+# 
+# # plot coefficients above .3
+# coefs_df %>% filter(abs(coef) > 0.3) %>% 
+#   ggplot(aes(x=var_name, y=coef)) +
+#   geom_bar(stat="identity") +
+#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+### ARCHIVED CODE
+
+# # I chose to set a "coronaKnow" variable which is 1 if the participants knows anyone with corona (including oneself)
+# # and is 0 if they do not. It's arguable if this is best, but gives us the most positive cases to work with.
+# df_mod_final <- df_analyse %>% mutate(knowcorona = factor(coronaclose == 6, labels = c("Yes", "No")))
+
+# create final data set for modelling and assign train, test, or validation set
+# CJ: We're already working with the training data.
+# set.seed(953007)
+# df_mod_final <- df_mod %>% 
+#   plyr::select(-coronaclose_1,-coronaclose_2,  -coronaclose_3,  -coronaclose_4,  -coronaclose_5, -coronaclose_6) %>% 
+#   mutate_at(vars_to_encode[12:17], funs(factor(.))) %>% 
+#   mutate(set = sample.int(3, nrow(df_mod), replace = TRUE, prob = c(p_train, p_test, p_validation)))
+
+# # ...
+# mean(predict(knowcorona_mod_lasso, newx = m_train, type="class", 
+#              s = knowcorona_mod_lasso$lambda.1se) == y_train) # train set
+# 
+# lasso_preds <- predict(knowcorona_mod_lasso, newx=m_train, type="class",
+#                        s=knowcorona_mod_lasso$lambda.1se)
+# 
+# lasso_preds_processed <- as.factor(unname(lasso_preds[,1]))
+# 
+# # Accuracy, precision, recall, F1...
+# precision <- posPredValue(lasso_preds_processed, y_train, positive="1")
+# precision
+# recall <- sensitivity(lasso_preds_processed, y_train, positive="1")
+# recall
+# F1 <- (2 * precision * recall) / (precision + recall)
+# F1
