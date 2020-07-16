238aec54ab94348ea70010db3685490faa51c031
c.j.vanlissa <c.j.vanlissa@gmail.com>
2020-07-16 14:51:59 +0000

    Partial dependence plots

--- scripts/label_vars.R
+++ scripts/label_vars.R
@@ -1,13 +1,12 @@
 df_vars <- read.csv("scripts/variable_names.csv", stringsAsFactors = FALSE)
-var_rename <- tolower(df_vars$X)
-names(var_rename) <- df_vars$lab
+df_vars$X <- tolower(df_vars$X)
+df_vars <- df_vars[df_vars$X %in% names(df_training), ]
+df_vars <- rbind(df_vars,
+                 data.frame(X = names(df_training)[!names(df_training) %in% df_vars$X],
+                            lab = NA))
 
-names(df_training)[names(df_training) %in% var_rename]
+write.csv(df_vars, "scripts/df_training_labs.csv", row.names = F)
 
-head(names(df_training)[!names(df_training) %in% var_rename])
-
-
-data.frame(X = c("countryiso3", "date", "coronaclose", "employstatus", "disc", 
-  "jbinsec"),
-  lab = c("Country", "Date", "COVID exposure", "Empl. status", "Soc. discontent", 
-          "Job insec.")
+df_vars <- read.csv("scripts/df_training_labs.csv", stringsAsFactors = F)
+var_rename <- tolower(df_vars$lab)
+names(var_rename) <- df_vars$X
